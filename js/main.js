2cf6cfeddb75bfc4d9d2c07ec762ab9aa5a2da04
ild <ildebrandoefrain@gmail.com>
2021-11-30 21:52:16 -0430

    hotel rombu

---
+++ js/main.js
@@ -0,0 +1,16 @@
+AOS.init({
+			offset:90,
+			duration: 800,
+			
+		})
+
+$(window).scroll(()=>{
+	console.log("scroll")
+	if (window.scrollY > 500){
+		console.log("entro")
+		$("nav.scroll-visible").addClass("show");
+	}
+	else if (window.scrollY < 500){
+		$("nav.scroll-visible").removeClass("show");
+	}
+})
